6635568e24fd99320fa1c3786df34cf63f00a317
Agentscreator <agentverse884@gmail.com>
2025-09-28 19:45:15 -0400

    Profile Pg

--- components/ProfilePage.tsx
+++ components/ProfilePage.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import type React from "react"
-
 import { useState, useEffect } from "react"
 import EventCard from "./EventCard"
 import ManageEventsToggle from "./ManageEventsToggle"
