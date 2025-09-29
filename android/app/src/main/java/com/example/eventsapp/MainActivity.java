a07c4db2037837a6c2ba6137dad56debc21633c7
Agentscreator <agentverse884@gmail.com>
2025-09-29 16:06:02 -0400

    Capacitor Download

---
+++ android/app/src/main/java/com/example/eventsapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.eventsapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
 
 export default nextConfig
