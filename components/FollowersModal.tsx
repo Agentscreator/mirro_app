570d48ddde0ba9ac637fbb15f73ae3f56328e1e8
Agentscreator <agentverse884@gmail.com>
2025-09-28 22:43:59 -0400

    cditing more prominent

--- components/FollowersModal.tsx
+++ components/FollowersModal.tsx
@@ -116,7 +116,7 @@ export default function FollowersModal({ isOpen, onClose, userId, type, currentU
             </h2>
             <button
               onClick={onClose}
-              className="p-2 text-taupe-400 hover:text-taupe-600 transition-colors duration-200"
+              className="p-2 bg-taupe-100 hover:bg-taupe-200 text-taupe-600 hover:text-taupe-800 rounded-full transition-all duration-200 hover:scale-105"
             >
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
